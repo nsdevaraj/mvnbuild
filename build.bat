c3379e43956787ae1e6f78db130afe1d30c77d32
N.S.Devaraj <nsdevaraj@gmail.com>
2016-09-28 15:25:31 +0530

    default build

--- build.bat
+++ build.bat
@@ -1,20 +1,30 @@
 @ECHO OFF
+for /D %%d in (*) do ( 
+	IF NOT "%%d"=="feature" IF NOT "%%d"=="p2repo" IF NOT "%%d"=="site" IF NOT "%%d"=="updatesite" set str1=%%d 
+	echo %%d | findstr /C:"feature">nul && (
+		REM with feature
+	) || (
+		set foldertarget=%str1%
+	)
+) 
+IF "%1"=="" ( SET "folder=%foldertarget%" ) ELSE ( SET "folder=%1" )
+IF "%2"=="" ( SET "ver=1.0" ) ELSE ( SET "ver=%2" )
 rem master pom
 copy pom.txt pom.xml
-copy cpom.txt %1\pom.xml
+copy cpom.txt %folder%\pom.xml
 rem copy feature files
-mkdir %1.feature
+mkdir %folder%.feature
 copy feature\category.txt feature\category.xml
 copy feature\feature.txt feature\feature.xml
 copy feature\pom.txt feature\pom.xml
-move feature\*.xml %1.feature\
-copy feature\build.properties %1.feature\build.properties
-copy %1\META-INF\MANIFEST.txt %1\META-INF\MANIFEST.MF /Y
+move feature\*.xml %folder%.feature\
+copy feature\build.properties %folder%.feature\build.properties
+copy %folder%\META-INF\MANIFEST.txt %folder%\META-INF\MANIFEST.MF /Y
 rem backup site
 mkdir updatesite
 copy site\category.txt site\category.xml
 copy site\pom.txt site\pom.xml
 move site\*.xml updatesite\
-PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%~dpn0.ps1' -compID %1 -version %2"
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%~dpn0.ps1' -compID %folder% -version %ver%"
 mvn clean install
-copy updatesite\target\Suite-%2.0.zip Suite-%2.0.zip
+copy updatesite\target\Suite-%ver%.0.zip Suite-%ver%.0.zip
